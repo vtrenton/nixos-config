b0477b2fb7a9f9d26fcbdb16fcdf21c389db58d9
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-11-19 11:30:37 -0600

    added some programs
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -85,6 +85,7 @@
     wget
     rsync
     gnupg
+    networkmanager-openconnect
     pinentry-tty
     transmission_4-gtk
     git
@@ -148,6 +149,20 @@
   #];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+
+  networking.firewall = {
+   # if packets are still dropped, they will show up in dmesg
+   logReversePathDrops = true;
+   # wireguard trips rpfilter up
+   extraCommands = ''
+     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
+   '';
+   extraStopCommands = ''
+     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
+   '';
+  }; 
 
   system.stateVersion = "24.05"; # Did you read the comment?
 }
