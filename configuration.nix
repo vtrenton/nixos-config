e7fe03b8712d4e2d5ba2efba8e36c4567096f8fd
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-03-03 22:47:51 -0600

    removed hostname and made more generic
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -10,7 +10,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "Krum"; # Define your hostname.
+  networking.hostName = ""; # Define your hostname.
 
   # Enable networking
   networking.networkmanager.enable = true;
