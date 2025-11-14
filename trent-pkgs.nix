89d59be2efa3582ed9e72b5d20d24c339faa8ee8
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-11-14 11:01:06 -0600

    added nix-init
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- trent-pkgs.nix
+++ trent-pkgs.nix
@@ -3,7 +3,7 @@ with pkgs; [
   # system
   tmux bat ghostty openvpn openssl xxd hexedit unzip p7zip jq yq-go devbox ipcalc 
   wireguard-tools virt-manager tree fzf system76-keyboard-configurator #rpi-imager
-  below transmission_4-gtk btop usbtop
+  below transmission_4-gtk btop usbtop nix-init
 
   # devel
   gh glab gcc gdb gnumake go golint errcheck yamllint cargo rustc ghc nodejs yarn
