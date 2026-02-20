db70614e47101ae3f37cebffdfe0d5cfb1b9e574
Trenton VanderWert <trenton.vanderwert@gmail.com>
2026-02-20 12:49:02 -0600

    added packer
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- trent-pkgs.nix
+++ trent-pkgs.nix
@@ -41,6 +41,6 @@ with pkgs; [
   # containers/DevOps
   kubectl kubernetes-helm minikube krew kubebuilder cri-tools clusterctl opentofu
   pulumi pulumi-esc pulumiPackages.pulumi-nodejs podman-compose azure-cli awscli2 
-  google-cloud-sdk google-cloud-sdk-gce ansible k0sctl dive kind teleport 
-  tailscale
+  google-cloud-sdk google-cloud-sdk-gce ansible k0sctl dive kind tailscale packer 
+  teleport
 ]
